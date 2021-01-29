d1ba0201dc3e1b7c1c054614e5893309ef35eabb
tk2rush90 <tk2rush90@naver.com>
2021-01-29 14:59:29 +0900

    change landing page

--- src/app/pages/post-list-page/post-list-page-routing.module.ts
+++ src/app/pages/post-list-page/post-list-page-routing.module.ts
@@ -2,17 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { PostListPageComponent } from './post-list-page.component';
-import {environment} from '../../../environments/environment';
-
-const {
-  categories,
-} = environment;
 
 const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
-    redirectTo: categories[0].value,
+    redirectTo: 'all',
   },
   {
     path: ':category',
